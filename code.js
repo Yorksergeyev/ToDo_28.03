dca065a2179b0f30c4389e192f6f850c2ed1233c
Yury Sergeyev <60914627+Yorksergeyev@users.noreply.github.com>
2020-04-01 18:14:01 +0300

    Add files via upload

--- code.js
+++ code.js
@@ -1,14 +1,10 @@
 import ItemDeal from "./ItemDeal";
 
-const motiv_arr = ["все победы начинаются с победы над самим собой", "just do it", "делу время, потехе час"]
+import {motiv_arr, ImportantArr, backgroundColor, MonthArray } from "./data.js"
+
+import anime from 'animejs/lib/anime.es.js';
 
 
-const ImportantArr = ['has-text-danger', 'has-text-warning', "has-text-success"]
-
-const backgroundColor = ['has-background-danger' ,'has-background-info' ,'has-background-primary'];
-
-
-const MonthArray = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня','Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря']
 
 const add_button = document.querySelector(".button-plus")
 const input = document.querySelector("input")
@@ -23,43 +19,49 @@ function getRand(arr){
     return mod_num;
 }
 
+
 function changePhrase(){
     document.querySelector("q").innerHTML = motiv_arr[getRand(motiv_arr)];
     // добавить сда анимации, с которой эта цитата появляется
 }
+anime({
+    targets: '.motiv',
+    translateX: anime.stagger(10, {grid: [14, 5], from: 'center', axis: 'x'}),
+    translateY: anime.stagger(10, {grid: [14, 5], from: 'center', axis: 'y'}),
+    rotateZ: anime.stagger([0, 90], {grid: [14, 5], from: 'center', axis: 'x'}),
+    delay: anime.stagger(200, {grid: [14, 5], from: 'center'}),
+    easing: 'easeInOutQuad'
+  });
 setInterval(changePhrase, 2000);
 
-function anim(){
-    let rand = Math.ceil(Math.random() * 3);
-    if(rand == 1){
-        document.getElementById('im').style.display = 'flex';
-        document.getElementById('im1').style.display = 'none';
-        document.getElementById('im2').style.display = 'none';
-    }
-    else if(rand == 2){
-        document.getElementById('im').style.display = 'none';
-        document.getElementById('im1').style.display = 'flex';
-        document.getElementById('im2').style.display = 'none';
-    }
-    else{
-        document.getElementById('im').style.display = 'none';
-        document.getElementById('im1').style.display = 'none';
-        document.getElementById('im2').style.display = 'flex';
-    }
-    
-}
-setInterval(anim, 1500);
 
 function createItem(){
     let text = input.value;
     if(text == ''){
         return; // return обрывает всю работу нашей функции
     }
-    // отдельная функция, которая делает отрисовку
+    let item = new ItemDeal(text, select.value - 1)
+    let item_to_JSON = JSON.stringify(item)
+
+    localStorage.setItem(+item.createAt, item_to_JSON)
+
+    DrawItem(item)// отдельная функция, которая делает отрисовку
     // сохранение информации в localStorage
     // упаковка информации
-    root.insertAdjacentHTML('afterBegin', `<div class="wrap-task field is-grouped">
-    <button class="button is-medium is-fullwidth ${backgroundColor[select.value - 1]}">  ${text}</button>
+    input.value = '';
+}
+
+
+
+
+
+
+function DrawItem(item){
+       root.insertAdjacentHTML('afterBegin', `<div class="wrap-task field is-grouped" id='${+item.createAt}'>
+    <button class="has-text-white button is-medium is-fullwidth ${backgroundColor[item.color]}">  ${item.text}
+    <span style='margin-left: 5vh;'>${item.createAt.getDate()} ${MonthArray[item.createAt.getMonth()]} </span>
+
+    </button>
     <button class="btn-delete button is-danger is-medium is-outlined">
         <span>Delete</span>
         <span class="icon is-small">
@@ -67,8 +69,35 @@ function createItem(){
         </span>
     </button>
 </div>`);
-    input.value = '';
+  anime({
+    targets: '.wrap-task',
+    translateY: 30,
+    scale: 1,
+    rotate: '1turn'
+  });
 }
+
+
+(function DrawOnLoad(){
+    for (let i = 0; i < localStorage.length; i++) {
+        let lk_key = localStorage.key(i) // получить ключ по номеру
+        let content = localStorage.getItem(lk_key);
+        let item = JSON.parse(content)
+   
+        let tempo_dat = Date.parse(item.createAt)
+     
+        item.createAt = new Date(tempo_dat)
+
+        DrawItem(item)
+    }
+})();
+
+
+
+
+
+
+
 add_button.addEventListener("click", createItem);
 
 input.addEventListener("keypress", (e)=>{
@@ -81,10 +110,17 @@ input.addEventListener("keypress", (e)=>{
 root.addEventListener("click", (e)=>{
     if(e.target.className == "fa fa-times"){
         var btn = e.target;
-
+        
     }
+    let deal = btn.parentNode.parentNode.parentNode;
+    anime({
+        targets: (deal),
+        translateY: 1000
+    });
     setTimeout(()=>{
-        btn.parentNode.parentNode.parentNode.remove();
+        deal.remove();
+        localStorage.removeItem(deal.id)
+        
     }, 300)
 })
 
@@ -116,8 +152,61 @@ select.onchange = () =>{
     ChangeColorEl(select);
 }
 
+document.querySelector('.start').onclick = function (){
+    function del(){
+        let elem = document.querySelector(".del");
+       elem.remove();
+    }setInterval(del, 10000);
+        root.insertAdjacentHTML('afterBegin', `<div class="del" style="display: grid; grid-template-columns: 200px 200px 200px; margin: 30px;">
+        <button class="is-dark " style="width:200px">JS 100+30 = :</button>
+        <input class="is-dark clown" style="width:50px"></input>
+        
 
+ 
+        </div>`)
 
+        anime({
+            targets: '.clown',
+            value: [0, 10030],
+            round: 1,
+            easing: 'easeInOutExpo'
+          });
+
+          anime({
+            targets: '.is-dark',
+            delay: anime.stagger(200, {grid: [14, 5], from: 'center'}),
+            
+            translateX: [
+              { value: 250, duration: 1000, delay: 500 },
+              { value: 0, duration: 1000, delay: 500 }
+            ],
+            translateY: 
+            [
+              { value: -40, duration: 500 },
+              { value: 40, duration: 500, delay: 1000 },
+              { value: 0, duration: 500, delay: 1000 }
+            ],
+            scaleX: [
+              { value: 4, duration: 100, delay: 500, easing: 'easeOutExpo' },
+              { value: 1, duration: 900 },
+              { value: 4, duration: 100, delay: 500, easing: 'easeOutExpo' },
+              { value: 1, duration: 900 }
+            ],
+            scaleY: [
+              { value: [1.75, 1], duration: 500 },
+              { value: 2, duration: 50, delay: 1000, easing: 'easeOutExpo' },
+              { value: 1, duration: 450 },
+              { value: 1.75, duration: 50, delay: 1000, easing: 'easeOutExpo' },
+              { value: 1, duration: 450 }
+            ],
+            easing: 'easeOutElastic(1, .8)',
+            loop: true
+          });
+          
+        }
+        
+
+        
 
 
 
